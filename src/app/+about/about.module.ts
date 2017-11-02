682a7fdcfd664cf30b71a6caf4de493ecf2d0e56
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2017-11-02 18:00:47 -0500

    Add navigation example

--- src/app/+about/about.module.ts
+++ src/app/+about/about.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { SHARED_MODULES, COMPONENT_DECLARATIONS } from './about.common';
+import { AboutComponent } from './components/about/about.component';
+import { IonicPageModule } from 'ionic-angular';
 
 @NgModule({
     imports: [
-        ...SHARED_MODULES,
+        IonicPageModule.forChild(AboutComponent),
+        ...SHARED_MODULES
     ],
     declarations: [
         ...COMPONENT_DECLARATIONS
