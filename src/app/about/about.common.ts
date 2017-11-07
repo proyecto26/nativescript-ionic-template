2194de929f7ba6099f21c5162c00aa9aef501747
Juan David Nicholls Cardona <jdnichollsc@hotmail.com>
2017-11-07 11:03:11 -0500

    Fix gitignore and update About component

--- src/app/about/about.common.ts
+++ src/app/about/about.common.ts
@@ -8,7 +8,6 @@ import { AboutComponent } from './components/about/about.component';
 
 export const SHARED_MODULES: any[] = [
     SharedModule,
-    RouterModule,
     RouterModule.forChild(AboutRoutes),
     TranslateModule.forChild()
 ];
